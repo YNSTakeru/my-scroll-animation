98787224179b03999aba0664904b5757a16e7014
YNS <95395710+YNSTakeru@users.noreply.github.com>
2022-04-22 13:32:29 +0900

    パスの調整

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const scene = new THREE.Scene();
 
 // 背景用のテクスチャ
 const textureLoader = new THREE.TextureLoader();
-const bgTexture = textureLoader.load("./bg/bg.JPG");
+const bgTexture = textureLoader.load("./bg/bg.jpeg");
 scene.background = bgTexture;
 
 // サイズ
